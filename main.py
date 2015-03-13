12006468d89684ce1db21b02446af8e4863c3216
Maxiee <maxieewong@gmail.com>
2015-03-13 20:29:49 +0800

    Hello world!

---
+++ main.py
@@ -0,0 +1,22 @@
+__author__ = 'Maxiee'
+
+from PyQt4 import QtCore, QtGui
+
+class MainWindow(QtGui.QWidget):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+
+        self.HelloLabel = QtGui.QLabel("Hello!")
+
+        mainLayout = QtGui.QVBoxLayout()
+        mainLayout.addWidget(self.HelloLabel)
+
+        self.setLayout(mainLayout)
+        self.setWindowTitle("Maxiee工具箱")
+
+if __name__ == '__main__':
+    import sys
+    app = QtGui.QApplication(sys.argv)
+    mainWindow = MainWindow()
+    mainWindow.show()
+    sys.exit(app.exec_())
