ba40c89ea3650224f6195ae53aba07471595b65f
Maxiee <maxieewong@gmail.com>
2015-03-20 12:35:04 +0800

    main.py: system tray.

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 __author__ = 'Maxiee'
 from PyQt4 import QtCore, QtGui
 import widgets.homepage.homepage
+import os
 
 
 class MainWindow(QtGui.QWidget):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+
+        self.myIcon = QtGui.QIcon(os.path.join(".", "MyData", "icon.jpg"))
+        self.icon = QtGui.QSystemTrayIcon()
+        self.icon.setIcon(self.myIcon)
+        self.icon.activated.connect(self.taryActivied)
+        self.icon.show()
+        self.setWindowIcon(self.myIcon)
 
         self.homepage = widgets.homepage.homepage.HomePage()
 
@@ -28,6 +36,21 @@ class MainWindow(QtGui.QWidget):
         self.setGeometry(100,100,300,600)
         self.setLayout(mainLayout)
         self.setWindowTitle("Maxiee工具箱")
+
+    def closeEvent(self, QCloseEvent):
+        reply = QtGui.QMessageBox.question(
+            self,  'Quit', 'Are you sure to quit?',
+            QtGui.QMessageBox.Yes|QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
+            QCloseEvent.accept()
+        else:
+            self.icon.show()
+            self.hide()
+            QCloseEvent.ignore()
+
+    def taryActivied(self, reason):
+        if reason == 2:
+            self.show()
 
 if __name__ == '__main__':
     import sys
