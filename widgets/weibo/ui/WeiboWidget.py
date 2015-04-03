2ddad44694b85fc54a517312425f6133e1168eb6
Maxiee <maxieewong@gmail.com>
2015-04-03 18:07:18 +0800

    fix a text format.

--- widgets/weibo/ui/WeiboWidget.py
+++ widgets/weibo/ui/WeiboWidget.py
@@ -6,11 +6,11 @@ class WeiboWidget(QtGui.QWidget):
     def __init__(self, parent=None, user="", text="", reUser=None, reText=None):
         super(WeiboWidget, self).__init__(parent)
         mainLayout = QtGui.QVBoxLayout()
-        weiboLabel = QtGui.QLabel(user + ":" + text)
+        weiboLabel = QtGui.QLabel(user + text)
         weiboLabel.setWordWrap(True)
         mainLayout.addWidget(weiboLabel)
         if reUser is not None or reText is not None:
-            reWeiboLabel = QtGui.QLabel(reUser + ":" + reText)
+            reWeiboLabel = QtGui.QLabel(reUser + reText)
             reWeiboLabel.setWordWrap(True)
             mainLayout.addWidget(reWeiboLabel)
         self.setLayout(mainLayout)
